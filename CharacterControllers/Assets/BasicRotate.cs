e81c0149b93159f89c8d173e3122a11cd3599221
Steve Smith <ssmith180@jccc.edu>
2018-01-29 17:39:06 -0600

    Added first person controller sandbox

---
+++ CharacterControllers/Assets/BasicRotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicRotate : MonoBehaviour {
+
+    public float rotationSpeed = 360f;  // Degrees per second
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(transform.up, rotationSpeed * Time.deltaTime, Space.World);
+		
+	}
+
+}
---
+++ CharacterControllers/Assets/FirstPersonControls.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonControls : MonoBehaviour {
+
+    public float speed = 4.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 moveDirection = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            //moveDirection += new Vector3(0, 0, 1);
+
+            moveDirection += transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveDirection += -transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveDirection += -transform.right;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveDirection += transform.right;
+        }
+
+        transform.position += moveDirection.normalized * speed * Time.deltaTime;
+
+	}
+}
